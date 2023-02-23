547c7991f5a0747be71f534a43ae4e7d75d4ca7f
Daniel Townsend <daniel.townsend@uon.edu.au>
2023-02-23 14:24:04 +1100

    adding resize element

--- SpellReflectShow.lua
+++ SpellReflectShow.lua
@@ -1,16 +1,34 @@
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 
-local myText = frame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-myText:SetPoint("CENTER")
-
+--Create frame and position
 local SRSwindow = CreateFrame("Frame", "SpellReflect Show", UIParent)
 SRSwindow:SetSize(200, 100)
 SRSwindow:SetPoint("CENTER")
 
+local myText = SRSwindow:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+myText:SetPoint("CENTER")
+
+-- Create the title text for the frame
+local title = SRSwindow:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+title:SetPoint("TOPLEFT", frame, "TOPLEFT", 5, -5)
+title:SetText("Spell Reflect Show")
+
 -- create a close button to hide the frame
 local closeButton = CreateFrame("Button", nil, SRSwindow, "UIPanelCloseButton")
 closeButton:SetPoint("TOPRIGHT", SRSwindow, "TOPRIGHT", -5, -5)
+
+-- Make the frame resizable
+SRSwindow:SetResizable(true)
+
+-- Set the minimum and maximum dimensions for the frame
+SRSwindow:SetMinResize(100, 50)
+SRSwindow:SetMaxResize(800, 600)
+
+-- Add a frame level event handler for the "OnSizeChanged" event
+SRSwindow:SetScript("OnSizeChanged", function(self, width, height)
+    myText:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -5, 5)
+end)
 
 -- create a slash command to show/hide the frame
 SLASH_SRS1 = "/srs"
@@ -33,7 +51,7 @@ SRSwindow:SetScript("OnMouseUp", function()
 end)
 
 -- capture the spell reflect
-frame:SetScript("OnEvent", function(self, event, ...)
+SRSwindow:SetScript("OnEvent", function(self, event, ...)
     if event == "COMBAT_LOG_EVENT_UNFILTERED" then
         local timestamp, eventType, _, sourceGUID, sourceName, sourceFlags, _, destGUID, destName, destFlags, _, spellID, spellName, _, extraSpellID, extraSpellName = ...
         if eventType == "SPELL_CAST_SUCCESS" and sourceGUID == UnitGUID("player") and spellID == 23920 then
