5556ee4f34aef3d11c189e9a9a5ffa2f1e97e93e
Daniel Townsend <daniel.townsend@uon.edu.au>
2023-02-24 08:59:36 +1100

    frame resizable, and moveable, created 2 sub zones

--- SpellReflectShow.lua
+++ SpellReflectShow.lua
@@ -9,11 +9,45 @@ local SRSwindow = CreateFrame("Frame", "SpellReflect Show", UIParent, "BasicFram
 SRSwindow:SetSize(200, 100)
 SRSwindow:SetPoint("CENTER")
 
+-- Create child frame for move frame
+local moveArea = CreateFrame("Button", nil, SRSwindow)
+moveArea:SetPoint("TOP", SRSwindow, "TOP", 0, 0)
+moveArea:SetSize(175, 20)
+moveArea:EnableMouse(true)
+moveArea:SetScript("OnMouseDown", function()
+    SRSwindow:SetMovable(true)
+    SRSwindow:StartMoving()
+end)
+moveArea:SetScript("OnMouseUp", function()
+    SRSwindow:SetMovable(false)
+    SRSwindow:StopMovingOrSizing()
+end)
+
+-- Create child frame for resize
+local resizeArea = CreateFrame("Button", nil, SRSwindow)
+resizeArea:SetPoint("BOTTOMRIGHT", SRSwindow, "BOTTOMRIGHT", 0, 0)
+resizeArea:SetSize(10, 10)
+resizeArea:EnableMouse(true)
+resizeArea:RegisterForDrag("LeftButton")
+SRSwindow:SetResizeBounds(200, 100, 400, 300)
+resizeArea:SetScript("OnDragStart", function()
+    SRSwindow:SetResizable(true)
+	SRSwindow:StartSizing()
+	SRSwindow.isMoving = true
+	SRSwindow.hasMoved = false
+end)
+resizeArea:SetScript("OnDragStop", function()
+    SRSwindow:SetResizable(false)
+	SRSwindow:StopMovingOrSizing()
+	SRSwindow.isMoving = false
+	SRSwindow.hasMoved = true
+end)
+
 SRSwindow:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 
+-- text test
 local myText = SRSwindow:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-myText:SetPoint("CENTER")
-myText:SetPoint("BOTTOMRIGHT", SRSwindow, "BOTTOMRIGHT", -5, 5)
+myText:SetPoint("TOPLEFT", SRSwindow, "TOPLEFT", 5, -30)
 myText:SetText("Spell Reflection: ... test")
 myText:Show()
 
@@ -22,15 +56,9 @@ local title = SRSwindow:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
 title:SetPoint("TOPLEFT", SRSwindow, "TOPLEFT", 5, -5)
 title:SetText("Spell Reflect Show")
 
--- Make the frame resizable
-SRSwindow:SetResizable(true)
-
--- Set the minimum and maximum dimensions for the frame
-SRSwindow:SetResizeBounds(100, 50, 800, 600)
-
 -- Add a frame level event handler for the "OnSizeChanged" event
 SRSwindow:SetScript("OnSizeChanged", function(self, width, height)
-    myText:SetPoint("BOTTOMRIGHT", SRSwindow, "BOTTOMRIGHT", -5, 5)
+    myText:SetPoint("TOPLEFT", SRSwindow, "TOPLEFT", 5, -20)
 end)
 
 -- create a slash command to show/hide the frame
@@ -43,7 +71,8 @@ SlashCmdList["SRS"] = function()
     end
 end
 
--- make the frame moveable
+-- make the frame moveable, this disables the resize
+--[[
 SRSwindow:SetMovable(true)
 SRSwindow:EnableMouse(true)
 SRSwindow:SetScript("OnMouseDown", function()
@@ -52,7 +81,7 @@ end)
 SRSwindow:SetScript("OnMouseUp", function()
     SRSwindow:StopMovingOrSizing()
 end)
-
+]]
 -- capture the spell reflect
 SRSwindow:SetScript("OnEvent", function(self, event, ...)
     if event == "COMBAT_LOG_EVENT_UNFILTERED" then
